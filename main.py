39aa6885eb8c9b771ec12e13bc5ad694a998ebf3
Kuldip Sahdeo <kshahdeo50@gmail.com>
2019-10-25 15:42:45 +0530

    code run

--- main.py
+++ main.py
@@ -1,1 +1,66 @@
+#Code to detect lane lines in a a image
 
+# importing following dependencies
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import matplotlib.image as mpimg
+
+#read and Show the image
+image_c = mpimg.imread('new_test_image_3.jpg')
+plt.imshow(image_c)  
+image_c.shape
+#convert to gray_scale image
+image_g = cv2.cvtColor(image_c, cv2.COLOR_RGB2GRAY)
+plt.imshow(image_g, cmap = 'gray')  
+image_g.shape
+#Gaussian Bluring and canny edge detection
+image_blurred = cv2.GaussianBlur(image_g, (7, 7), 0)
+plt.imshow(image_blurred) 
+
+
+threshold_low = 10
+threshold_high = 200
+
+image_canny = cv2.Canny(image_blurred, threshold_low, threshold_high)
+plt.imshow(image_canny, cmap = 'gray')  
+
+# Visualize the region of interest
+vertices = np.array([[(20,460),(340, 300), (460, 300), (740,460)]], dtype=np.int32)    
+mask = np.zeros_like(image_g)   
+cv2.fillPoly(mask, vertices, 255)
+masked_image = cv2.bitwise_and(image_g, mask)
+plt.figure()
+plt.imshow(masked_image)
+
+masked_image = cv2.bitwise_and(image_canny, mask)
+plt.figure()
+plt.imshow(masked_image)
+
+#Hough Line detection and drawing function
+
+rho = 2            # distance resolution in pixels 
+theta = np.pi/180  # angular resolution in radians 
+threshold = 40     # minimum number of votes 
+min_line_len = 100  # minimum number of pixels making up a line
+max_line_gap = 50  # maximum gap in pixels between connectable line segments    
+lines = cv2.HoughLinesP(masked_image, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    
+# Create an empty black image
+line_image = np.zeros((masked_image.shape[0], masked_image.shape[1], 3), dtype=np.uint8)
+    
+for line in lines:
+    for x1,y1,x2,y2 in line:      
+        cv2.line(line_image, (x1, y1), (x2, y2), [255, 0, 0], 20)
+lines 
+
+α = 1
+β = 1
+γ = 0    
+
+# Resultant weighted image is calculated as follows: original_img * α + img * β + γ
+Image_with_lines = cv2.addWeighted(image_c, α, line_image, β, γ)
+
+
+plt.figure()
+plt.imshow(Image_with_lines)
